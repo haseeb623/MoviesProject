782ffe3f6e0aa3030cd0f7330a9ce91c7c1b4f00
Haseeb <haseebiftikhar21@gmail.com>
2021-11-23 05:50:01 -0800

    commit

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,22 @@
+<link rel="shortcut icon" href="{{asset('front/assets/images/website/favicon.ico')}}">
+<link rel="apple-touch-icon" sizes="57x57" href="{{asset('front/assets/images/website/apple-touch-icon-57x57.png')}}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{asset('front/assets/images/website/apple-touch-icon-114x114.png')}}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{asset('front/assets/images/website/apple-touch-icon-72x72.png')}}">
+<link rel="apple-touch-icon" sizes="144x144" href="{{asset('front/assets/images/website/apple-touch-icon-144x144.png')}}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{asset('front/assets/images/website/apple-touch-icon-60x60.png')}}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{asset('front/assets/images/website/apple-touch-icon-120x120.png')}}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{asset('front/assets/images/website/apple-touch-icon-76x76.png')}}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{asset('front/assets/images/website/apple-touch-icon-152x152.png')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{asset('front/assets/images/website/apple-touch-icon-180x180.png')}}">
+<link rel="icon" type="image/png" href="{{asset('front/assets/images/website/favicon-160x160.png')}}" sizes="160x160">
+<link rel="icon" type="image/png" href="{{asset('front/assets/images/website/favicon-96x96.png')}}" sizes="96x96">
+<link rel="icon" type="image/png" href="{{asset('front/assets/images/website/favicon-16x16.png')}}" sizes="16x16">
+<link rel="icon" type="image/png" href="{{asset('front/assets/images/website/favicon-32x32.png')}}" sizes="32x32">
+<!-- <meta name="msapplication-TileColor" content="#00a300">
+<meta name="msapplication-TileImage" content="{{asset('front/assets/images/website/mstile-144x144.png')}}">
+<meta name="msapplication-config" content="{{asset('front/assets/images/website/browserconfig.xml')}}"> -->
+<link rel="search" type="application/opensearchdescription+xml" href="{{asset('front/opensearch.xml')}}" title="YTS - The Official Home of YIFY Movie Torrent Downloads" />
+<link href='https://fonts.googleapis.com/css?family=Arimo:400,700,400italic,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="{{asset('front/assets/fonts/fonts.css')}}">
+<link rel="stylesheet" href="{{asset('front/assets/minified/2bacb5de663bc55f0cbed2b4a7d1504dcc81e.css?2')}}">
+<link rel="stylesheet" href="other.css">
---
+++ resources/views/layout/script.blade.php
@@ -0,0 +1,22 @@
+<script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "WebSite",
+            "url": "https://yts.mx/",
+            "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://yts.mx/browse-movies/{search_term_string}/all/all/0/latest",
+                "query-input": "required name=search_term_string"
+            }
+        }
+        </script>
+<!-- <script async src='cdn-cgi/bm/cv/669835187/api.js'></script> -->
+<script type="text/javascript" src="{{asset('front/assets/minified/modded13bb0.js?yify=4')}}"></script>
+<script type="text/javascript" src="{{asset('front/../s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56896aba6888ef3e')}}" async="async"></script>
+<script type="text/javascript" src="{{asset('front/../fbcdn2.com/script/su.js')}}" zid="1221179" admn="bG5hcmFxX3Bieg==" admc="b3lyYmN2X3Bieg==" did="UDVol"></script>
+<script type="text/javascript">
+(function() {setTimeout(function(){if(!window["s2ss1115"]){let e=document.querySelector('script[did="UDVol"]'),t=!!e.hasAttribute("admc")&&function(e){let t=atob(e).replace(/[a-zA-Z]/g,function(e){var t=e<="Z"?65:97;return String.fromCharCode(t+(e.charCodeAt(0)-t+13)%26)});return t.replaceAll("_",".")}(e.getAttribute("admc")),r=e.getAttribute("src"),a=e.parentElement,c=document.createElement("script");!function(e,t){let r,a=["did","admc","src"],c=Array.prototype.slice.call(t.attributes);for(;r=c.pop();)-1===a.indexOf(r.nodeName)&&e.setAttribute(r.nodeName,r.nodeValue)}(c,e),c.src=(d=r,"//"+t+d.replace(/^.*\/\/[^\/]+/,"")),c.setAttribute("sadbl",1),a.removeChild(e),a.appendChild(c)}var d},150);document.body.addEventListener("click",function(e){window["s2ss1115ff"]instanceof Function&&window["s2ss1115ff"](e)});})();
+</script>
+<script type="text/javascript">(function(){window['__CF$cv$params']={r:'6ae8cfec1d6d14e5',m:'.bRzK4n6iB0fh48lbKC_GES1p8Z1FFCWJF_WXF65dds-1636982797-0-AWoAv+b0bhJn2in/r+tqIqgBu0m5y6xg5T2iOrizXA6ntzRBJva/O4ihkUtD3wxes/NY2uwJ8Cyo+vF7Xi17kz2X2AaTqA3mD51fG5yUC5r9WQcOW6VLhGcze84ccnPeEw==',s:[0xd2517d224d,0xc650465d3e],}})();</script>
+<script defer src="{{asset('front/../static.cloudflareinsights.com/beacon.min.js')}}" data-cf-beacon='{"rayId":"6ae8cfec1d6d14e5","token":"73ccc363e9074a98b0411dc1a6565531","version":"2021.11.0","si":100}' crossorigin="anonymous"></script>
+
