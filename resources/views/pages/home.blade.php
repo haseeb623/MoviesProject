5177dab83bc7a1c6725382663cbb7cc34c67573b
Haseeb <haseebiftikhar21@gmail.com>
2021-11-24 06:11:03 -0800

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -448,6 +448,7 @@
             <div class="row movie-list">
                <h2>Latest HW Movies  <a href="browse-movies.html">Browse All</a></h2>
             </div>
+           
             <div class="row">
              @foreach($moviesArray as $val)
                <div class="browse-movie-wrap col-xs-10 col-sm-5">
