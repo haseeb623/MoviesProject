b0d45f1628fd64d4f3f63af1efa6cabc73785a1c
zulqii777 <80761391+zulqii777@users.noreply.github.com>
2021-11-25 22:41:20 -0800

    change
    lkklkm
    
    Co-Authored-By: haseeb623 <70386639+haseeb623@users.noreply.github.com>

---
+++ app/Http/Controllers/UpcomingMovies (2).php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class UpcomingMovies extends Controller
+{
+    //
+    function index(){
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://yts.mx/api/v2/list_upcoming.json?limit=4',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Cookie: PHPSESSID=vvu7s47s6cj2gn488spu941dag'
+        ),
+        ));
+        $response = curl_exec($curl);
+    
+        curl_close($curl);
+    
+        $responseDecoded = json_decode($response,TRUE);
+        $moviesArray = $responseDecoded['data']['movies'];
+        return view('pages.home',compact('upcoming'));
+        }
+}
---
+++ resources/views/layout/nav.blade (2).php
@@ -0,0 +1,30 @@
+</header class="nav-bar" style="nav-bar">
+<div class="nav-logo pull-left">
+<a href="#"><img src="https://st4.depositphotos.com/28687978/41102/v/380/depositphotos_411023408-stock-illustration-logo-design-white-letter-letter.jpg" alt=""></a>
+</div>
+<span class="header-slogan hidden-xs pull-left">HD movies at the smallest file size.</span>
+<div class="main-nav-links hidden-sm hidden-xs">
+<form method="GET" action="https://yts.mx/search-movies" accept-charset="UTF-8" id="quick-search" name="quick-search">
+<div id="quick-search-container">
+<input id="quick-search-input" name="query" autocomplete="off" type="search" value="Quick search">
+<div class="ajax-spinner"></div>
+</div>
+</form>
+<ul class="nav-links">
+<li><a href="index.html"> Home </a></li>
+<li><a href="browse-movies/0/2160p/all/0/latest/0/all.html" style="color: #6AC045"> 4K </a></li>
+<li><a href="trending-movies.html"> Trending </a></li>
+<li><a href="browse-movies.html"> Browse Movies </a></li>
+</ul>
+<ul class="nav-links nav-link-guest">
+<li><a class="login-nav-btn" href="javascript:void(0)"> Login </a> &nbsp;|&nbsp; <a class="register-nav-btn" href="javascript:void(0)"> Register </a></li>
+</ul>
+</div>
+<div class="nav-mobile-buttons hidden-md hidden-lg">
+<a class="touchable" id="mobile-search-btn" href="javascript:void(0)"> <span class="glyphicon glyphicon-search"></span> </a>
+<a class="touchable" href="browse-movies/0/2160p/all/0/latest/0/all.html" style="color: #6AC045"> <b>4K</b> </a>
+<a class="touchable" href="trending-movies.html"> <span class="glyphicon glyphicon-stats"></span> </a>
+<a class="touchable" href="browse-movies.html"> <span class="glyphicon glyphicon-list-alt"></span> </a>
+<a class="touchable" href="login.html"> <span class="glyphicon glyphicon-user"></span> </a>
+</div>
+</header>
